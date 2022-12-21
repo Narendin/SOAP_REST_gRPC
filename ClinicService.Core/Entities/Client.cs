6cbe97465754e041b3e17d9aada60cc1038efca4
VladimirDolguev <v.dolguev@vekas.org>
2022-12-21 19:18:29 +0300

    feat(): Урок 3. Введение в gRPC

---
+++ ClinicService.Core/Entities/Client.cs
@@ -0,0 +1,32 @@
+﻿using ClinicService.Core.Entities.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicService.Core.Entities
+{
+    [Table("Clients")]
+    public class Client : BaseEntity<Guid>
+    {
+        [Column]
+        [StringLength(50)]
+        public string? Document { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? SurName { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? FirstName { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? Patronymic { get; set; }
+
+        [InverseProperty(nameof(Pet.Client))]
+        public ICollection<Pet> Pets { get; set; } = new HashSet<Pet>();
+
+        [InverseProperty(nameof(Consultation.Client))]
+        public ICollection<Consultation> Consultations { get; set; } = new HashSet<Consultation>();
+    }
+}
---
+++ ClinicService.Data/EF/ApplicationContextHelper.cs
@@ -0,0 +1,42 @@
+﻿using ClinicService.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ClinicService.Data.EF
+{
+    public static class ApplicationContextHelper
+    {
+        public static IConfiguration GetConfiguration()
+        {
+            // получаем конфигурацию
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile(Constants.AppSettingsJSON, true);
+
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (File.Exists($"appsettings.{environmentName}.json"))
+                builder.AddJsonFile($"appsettings.{environmentName}.json", true);
+
+            IConfiguration config = builder.Build();
+            return config;
+        }
+
+        public static DbContextOptionsBuilder ConfigureDbContextOptions(DbContextOptionsBuilder optionsBuilder, IConfiguration configuration)
+        {
+            string? connectionString = configuration[Constants.SQLProvider.ConnectionStringPath];
+
+            return ConfigureDbContextOptions(optionsBuilder, connectionString);
+        }
+
+        public static DbContextOptionsBuilder ConfigureDbContextOptions(DbContextOptionsBuilder optionsBuilder, string? connectionString, string? sqlServerProvider = null)
+        {
+            optionsBuilder.UseSqlServer(connectionString, bo =>
+            {
+                bo.CommandTimeout(200);
+                bo.MigrationsAssembly("ClinicService.Data.MSSQL");
+            });
+
+            return optionsBuilder;
+        }
+    }
+}
